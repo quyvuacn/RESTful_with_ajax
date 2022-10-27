1ec202d650bed35b74311010663a9e1fe949b11d
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-10-27 19:34:34 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 8000
+const port = process.env.PORT ||8000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded())
